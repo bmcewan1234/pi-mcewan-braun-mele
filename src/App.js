57e3fc0ec8b234eb7e24417b18450a6dd6527f2c
bmcewan1234 <bmcewan@udesa.edu.ar>
2026-04-15 11:05:21 -0300

    linkie login register y buscador

--- src/App.js
+++ src/App.js
@@ -3,18 +3,30 @@ import Header from "./Componentes/Header/Header";
 import Footer from "./Componentes/Footer/Footer";
 import Home from "./Screen/Home/Home";
 import Buscador from "./Componentes/Buscador/Buscador";
+import Login from "./Screen/login/login";
+import Register from "./Screen/register/register";
+import { Route, Switch } from "react-router-dom/cjs/react-router-dom.min";
 
 
 
 
 function App() {
   return (
-    <React.Fragment>
+    <>
       <Header />
-      <Home />
-      <Buscador />
+      <Switch>
+        <Route path="/register" component={Register} />
+        <Route path="/login" component={Login} />
+        <Route path="/search/" component={Buscador} />
+        
+        <Home />
+        <Buscador />
+        <Login />
+        <Register />
+
+      </Switch>
       <Footer />
-    </React.Fragment>
+    </>
   );
 }
 
