a95cb4336346e2203ac8668e231344f5ae435235
ibrauni <ibraun@udesa.edu.ar>
2026-05-13 18:35:38 -0300

    Cambie el componente cardPeli a funciones usando useState y useEffect

--- src/Componentes/Cardpeli/Cardpeli.js
+++ src/Componentes/Cardpeli/Cardpeli.js
@@ -1,106 +1,101 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
 
-class CardPeli extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            ver: "Ver descripcion",
-            clase: "no-mostrar",
-            favorito: false
-        }
-    }
+const CardPeli = (props) => {
+    const [ver, setVer] = useState("Ver descripcion");
+    const [clase, setClase] = useState("no-mostrar");
+    const [favorito, setFavorito] = useState(false);
 
-    ocultar() {
-        if (this.state.ver === "Ver descripcion") {
-            this.setState({
-                ver: "Ocultar descripcion",
-                clase: "mostrar"
-            })
+    useEffect(() => {
+        let storage = localStorage.getItem("favoritosP");
+        if (storage !== null) {
+            let storageParseado = JSON.parse(storage);
+            let estaEnFav = storageParseado.includes(Number(props.pelicula.id));
+            setFavorito(estaEnFav);
         }
-        else {
-            this.setState({
-                ver: "Ver descripcion",
-                clase: "no-mostrar"
-            })
-        }
-    }
+    }, [props.pelicula.id]);
 
-    agregarFavP() {
-        let idFav = this.props.pelicula.id
-        let storage = localStorage.getItem("favoritosP")
-        if (storage != null) {
-            let storageParse = JSON.parse(storage)
-            storageParse.push(idFav)
-            let storageString = JSON.stringify(storageParse)
-            localStorage.setItem("favoritosP", storageString)
-            this.setState({ favorito: true })
+    const ocultar = () => {
+        if (ver === "Ver descripcion") {
+            setVer("Ocultar descripcion");
+            setClase("mostrar");
         } else {
-            let arrayIDs = []
-            arrayIDs.push(idFav)
-            let arrayString = JSON.stringify(arrayIDs)
-            localStorage.setItem("favoritosP", arrayString)
-            this.setState({ favorito: true })
+            setVer("Ver descripcion");
+            setClase("no-mostrar");
         }
+    };
 
-    }
+    const agregarFavP = () => {
+        let idFav = props.pelicula.id;
+        let storage = localStorage.getItem("favoritosP");
+        if (storage != null) {
+            let storageParse = JSON.parse(storage);
+            storageParse.push(idFav);
+            let storageString = JSON.stringify(storageParse);
+            localStorage.setItem("favoritosP", storageString);
+            setFavorito(true);
+        } else {
+            let arrayIDs = [];
+            arrayIDs.push(idFav);
+            let arrayString = JSON.stringify(arrayIDs);
+            localStorage.setItem("favoritosP", arrayString);
+            setFavorito(true);
+        }
+    };
 
-    sacarFavP() {
-        let idFav = this.props.pelicula.id
-        let storage = localStorage.getItem("favoritosP")
+    const sacarFavP = () => {
+        let idFav = props.pelicula.id;
+        let storage = localStorage.getItem("favoritosP");
         if (storage !== null) {
-            let storageParseado = JSON.parse(storage)
-            let storageFiltrado = storageParseado.filter(id => id !== idFav)
-            let storageString = JSON.stringify(storageFiltrado)
-            localStorage.setItem("favoritosP", storageString)
-            this.setState({ favorito: false })
+            let storageParseado = JSON.parse(storage);
+            let storageFiltrado = storageParseado.filter(id => id !== idFav);
+            let storageString = JSON.stringify(storageFiltrado);
+            localStorage.setItem("favoritosP", storageString);
+            setFavorito(false);
         }
-    }
-    componentDidMount() {
-        let storage = localStorage.getItem("favoritosP")
-        if (storage !== null) {
-            let storageParseado = JSON.parse(storage)
-            let estaEnFav = storageParseado.includes(Number(this.props.pelicula.id))
+    };
 
-            this.setState({
-                favorito: estaEnFav
-            })
-        }
-    }
+    return (
+        <article className="single-card-movie">
+            <img src={"https://image.tmdb.org/t/p/w342/" + props.pelicula.poster_path} alt={props.pelicula.title} />
 
-    render() {
-        return (
-            <article className="single-card-movie">
-                <img src={"https://image.tmdb.org/t/p/w342/" + this.props.pelicula.poster_path} alt={this.props.pelicula.title} />
+            <div className="cardBody">
+                <h2>{props.pelicula.title}</h2>
 
-                <div className="cardBody">
-                    <h2>{this.props.pelicula.title}</h2>
-
-                    <div className="card-buttons">
-                        <button onClick={() => this.ocultar()}>{this.state.ver}</button>
-                    </div>
-
-                    <div className={this.state.clase + " card-text"}>
-                        <p>{this.props.pelicula.overview}</p>
-                    </div>
-
-                    <div className="card-buttons">
-                        <Link to={this.props.tipo === "tv" ? `/detalleSerie/${this.props.pelicula.id}` : `/detallePeli/${this.props.pelicula.id}`}>
-                            <button className="btn btn-primary">Ir a detalle</button>
-                        </Link>
-                    </div>
-                    <div className="card-buttons">
-                        <button
-                            className={this.state.favorito === true ? "no-mostrar" : ""}
-                            onClick={() => this.agregarFavP()} > Agregar a Favoritos </button>
-                        <button
-                            className={this.state.favorito === false ? "no-mostrar" : ""}
-                            onClick={() => this.sacarFavP()} > Quitar de Favoritos </button>
-                    </div>
+                <div className="card-buttons">
+                    <button onClick={ocultar}>{ver}</button>
                 </div>
-            </article>
-        )
-    }
-}
 
-export default CardPeli
+                <div className={clase + " card-text"}>
+                    <p>{props.pelicula.overview}</p>
+                </div>
+
+                <div className="card-buttons">
+                    <Link to={props.tipo === "tv" ? `/detalleSerie/${props.pelicula.id}` : `/detallePeli/${props.pelicula.id}`}>
+                        <button className="btn btn-primary">Ir a detalle</button>
+                    </Link>
+                </div>
+                <div className="card-buttons">
+                    <button
+                        className={favorito === true ? "no-mostrar" : ""}
+                        onClick={agregarFavP}
+                    >
+                        Agregar a Favoritos
+                    </button>
+                    <button
+                        className={favorito === false ? "no-mostrar" : ""}
+                        onClick={sacarFavP}
+                    >
+                        Quitar de Favoritos
+                    </button>
+                </div>
+            </div>
+        </article>
+    );
+};
+
+export default CardPeli;
+
+
+    
